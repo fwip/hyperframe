5fb9ed490f915baeb50eeff1c19c06c8bd415664
Jemma Nelson <pink.fwip@gmail.com>
2020-04-11 16:22:33 -0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,31 +1,66 @@
 //require('require-rebuild')();
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const diff = require('deep-diff')
+
+app.allowRendererProcessReuse = false;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
 }
 
-const { Repo } = require('hypermerge')
+const { RepoBackend } = require('hypermerge')
 const Hyperswarm = require('hyperswarm')
+const hyperswarm = Hyperswarm()
 
-const pth = '.data'
+//const pth = '.data'
 
-const repo = new Repo({ pth })
+//const repo = new Repo({ pth })
 
-repo.setSwarm(Hyperswarm())
 
-const createWindow = () => {
+//repo.setSwarm(Hyperswarm())
+
+//let docUrl = repo.create()
+
+/*
+ipcMain.on('content-update', (event, arg) => {
+  console.log('---')
+  // console.log(event)
+  //console.log(arg.attrdiff)
+
+  repo.change(docUrl, (state) => {
+    state = state || {}
+    let node = state[arg.node] || {}
+    arg.attrdiff.forEach((change) => {
+      diff.applyChange(node, '', change)
+    })
+    state[arg.node] = node
+  })
+})
+*/
+
+const createWindow = (storagePath = ".pth2", port = 12049, url = undefined) => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 800,
+    width: 800,
+    height: 600,
     show: false,
     webPreferences: {
       nodeIntegration: true,
     }
   });
+
+  const back = new RepoBackend({ path: storagePath })
+  back.setSwarm(Hyperswarm())
+  back.subscribe((msg) => {
+    // console.log("sending to frontend", msg)
+    mainWindow.webContents.send('toFront', msg)
+  })
+  ipcMain.on('toBack', (event, message) => {
+    // console.log("updating backend", message)
+    back.receive(message)
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
@@ -33,9 +68,9 @@ const createWindow = () => {
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
     setTimeout(() => {
-      mainWindow.webContents.send('message', { 'debug': 'hello' })
+      mainWindow.webContents.send('scene-change', { 'url': url })
     },
-      3000)
+      1000)
   })
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
@@ -44,7 +79,24 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => {
+  let url = "hypermerge:/FwevzvpExWvujDndgYPmxchLGTmgAyz5GdDfecvYgijT"
+  url = null
+  createWindow(".pth8", 0, url)
+  return
+  ipcMain.on('scene-loaded', (event, message) => {
+    console.log("loaded scene: ", message.url)
+
+    setTimeout(() => {
+      console.log("making new window")
+      createWindow(".pth1", 0, message.url)
+    },
+      3000
+    )
+
+  })
+
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -55,6 +107,7 @@ app.on('window-all-closed', () => {
   }
 });
 
+/*
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
@@ -62,6 +115,7 @@ app.on('activate', () => {
     createWindow();
   }
 });
+*/
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
